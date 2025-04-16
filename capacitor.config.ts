cdb26f4b3b5ec140d8b30b480cdb6110af497a4f
tushermahmud <=>
2025-04-16 17:44:24 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'goodgreencordovanew',
+  webDir: 'www'
+};
+
+export default config;
